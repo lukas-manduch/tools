96ceb13802537b7decea1274249c0fbc5a0c8450
Lukas Manduch <lukas.manduch@eset.sk>
2017-10-31 14:06:37 +0100

    Set global-visual-line mode
    This should avoid freeze on long lines

--- emacs.el
+++ emacs.el
@@ -14,9 +14,10 @@
  '(ansi-color-names-vector
    ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
  '(custom-enabled-themes (quote (deeper-blue)))
- '(dired-x-hands-off-my-keys nil)
  '(dired-recursive-copies (quote always))
  '(dired-recursive-deletes (quote always))
+ '(dired-x-hands-off-my-keys nil)
+ '(global-visual-line-mode t)
  '(ibuffer-saved-filter-groups
    (quote
     (("Files & Folders"
@@ -44,9 +45,8 @@
         (mode . lisp-mode)))))))
  '(ido-enable-flex-matching t)
  '(ido-everywhere t)
- '(package-selected-packages (quote (nhexl-mode)))
- '(show-trailing-whitespace t) ; Highlight trailing whitespace errors
- )
+ '(package-selected-packages (quote (dash nhexl-mode)))
+ '(show-trailing-whitespace t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
