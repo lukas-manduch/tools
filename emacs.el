0ec43342f223ebc8f26d5eaa070ce998d7269aa0
Lukas Manduch <manduch.lukas@gmail.com>
2017-11-05 18:21:53 +0100

    Function for fast switching terminals

--- emacs.el
+++ emacs.el
@@ -275,3 +275,18 @@
         (ediff3 (buffer-file-name (nth 0 marked-files))
                 (buffer-file-name (nth 1 marked-files))
                 (buffer-file-name (nth 2 marked-files)))))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Open shell fast
+(defun new-shell (name)
+  "Opens a new shell buffer with the given name in
+asterisks (*name*) in the current directory and changes the
+prompt to 'name>'."
+  (interactive "sName: ")
+  (pop-to-buffer (concat "*" name "*"))
+  (unless (eq major-mode 'shell-mode)
+    (shell (current-buffer))
+    (sleep-for 0 200)
+    (delete-region (point-min) (point-max))
+    (comint-simple-send (get-buffer-process (current-buffer))
+                        (concat "export PS1=\"\033[33m" name "\033[0m:\033[35m\\W\033[0m>\""))))
+(global-set-key (kbd "C-c s") 'new-shell)
