387ebcc76a7e6657026ef3957cdfd427083116a5
Lukas Manduch <manduch.lukas@gmail.com>
2022-01-18 14:59:23 +0100

    Start sqlite parser

---
+++ sqlite_parser/Header.cs
@@ -0,0 +1,44 @@
+﻿namespace SqliteParser;
+
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential, Pack = 0)]
+internal unsafe struct SqliteUnsafeHeader
+{
+    public fixed byte Heading[16];
+    public fixed byte Rest[84];
+}
+
+struct SqliteHeader
+{
+    public string Heading;
+}
+
+class Header
+{
+    public static unsafe SqliteHeader FromBytes(byte [] bytes)
+    {
+        if (bytes == null || bytes.Length != 100)
+        {
+            throw new ArgumentException(nameof(bytes), "Must be exactly 100 bytes");
+        }
+
+        
+        var header = new SqliteUnsafeHeader();
+        byte* p = (byte*)&header;
+        Debug.Assert(bytes.Length == sizeof(SqliteUnsafeHeader), "Size of Sqlite header is wrong");
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            p[i] = bytes[i];
+        }
+
+
+        Console.WriteLine("Size is {0}", sizeof(SqliteUnsafeHeader));
+        return new SqliteHeader()
+        {
+            Heading = System.Text.Encoding.Default.GetString(header.Heading, 16)
+        };
+    }
+}
+
---
+++ sqlite_parser/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace SqliteParser
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Wrong number of args. Pass sqlite file as arg 1");
+                Environment.Exit(1);
+            }
+            
+            try
+            {
+                byte[] headerBytes = new byte[100];
+
+                using (var fileStream = File.Open(args[0], FileMode.Open))
+                {
+                    using (var binaryReader = new BinaryReader(fileStream))
+                    {
+                        binaryReader.Read(headerBytes, 0, 100);
+                    }
+                }
+                var header = Header.FromBytes(headerBytes);
+                Console.WriteLine($"{header.Heading} ");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error while processing file '{args[0]}': {ex.Message}");
+            }
+            Environment.Exit(0);
+        }
+    }
+}
