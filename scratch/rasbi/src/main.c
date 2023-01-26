10988e3a78dacedcf2e6e418c0e32c13006bd095
Lukas Manduch <manduch.lukas@gmail.com>
2023-01-26 22:50:13 +0000

    Rasbi - string parser init version

--- scratch/rasbi/src/main.c
+++ scratch/rasbi/src/main.c
@@ -1,14 +1,61 @@
-#include <unistd.h>
 #include <sys/types.h>
-#include <stdio.h>
 
+#define DEBUG 1
+#define NULL 0
+#define FALSE 0
+#define TRUE 1
 #define STACK_SIZE 4096
 #define u64 unsigned long long
+#define u32 unsigned int
+#define i32 int
 #define i64 signed long long
 
 _Static_assert(sizeof(u64) == 8, "Bad size");
 _Static_assert(sizeof(i64) == 8, "Bad size");
+_Static_assert(sizeof(u32) == 4, "Bad size");
+_Static_assert(sizeof(i32) == 4, "Bad size");
 
+i64 sys_read(u32 fd, const char *buf, u64 count) {
+	i64 ret = 0;
+ 	__asm__ volatile (
+ 	        "movq $0, %%rax\n\t"
+ 	        "movl %1, %%edi\n\t"
+ 	        "movq %2, %%rsi\n\t"
+ 	        "movq %3, %%rdx\n\t"
+		"syscall\n\t"
+		"movq %%rax, %0 \n\t"
+ 		 : "=rm" (ret)
+ 		 : "rm"  (fd), "rm" (buf), "rm" (count)
+ 		 : "rax" , "rdi", "rsi", "rdx" /* These two idk */, "r11", "rcx"
+ 	       );
+	return ret;
+}
+
+void sys_exit(i32 error_code) {
+	__asm__ (
+		"movq $60, %%rax\n\t"
+		"movl %0, %%edi\n\t"
+		"syscall"
+		: // empty
+		: "rm" (error_code)
+		);
+}
+
+i64 sys_write(u32 fd, const char *buf, u64 count) {
+	i64 ret = 0;
+ 	__asm__ volatile (
+ 	        "movq $1, %%rax\n\t"
+ 	        "movl %1, %%edi\n\t"
+ 	        "movq %2, %%rsi\n\t"
+ 	        "movq %3, %%rdx\n\t"
+		"syscall\n\t"
+		"movq %%rax, %0 \n\t"
+ 		 : "=rm" (ret)
+ 		 : "rm"  (fd), "rm" (buf), "rm" (count)
+ 		 : "rax" , "rdi", "rsi", "rdx" /* These two idk */, "r11", "rcx"
+ 	       );
+	return ret;
+}
 #if __STDC_VERSION__ != 201710L
 #error "Bad c version, use std=c17"
 #endif
@@ -26,48 +73,214 @@ void init_context(struct context* ctx) {
 	ctx->_stack_pointer = STACK_SIZE;
 }
 
-struct Expression {
-
+enum ExpressionType {
+	SYMBOL   = 1 << 1, 
+	STRING   = 1 << 2,
+	LIST     = 1 << 3,
+	NUMBER   = 1 << 4,
+	FUNCTION = 1 << 5,
 };
 
-void* stack_push(struct context* ctx, u64 value) {
+struct StringExpression {
+	u32 size;
+	char content[];
+};
 
+typedef struct ExpressionT {
+	u32 expr_type;
+	u32 pad;
+	union {
+		u64 value64;
+		struct StringExpression value_string;
+	};
+} Expression;
+
+
+
+#if DEBUG == 0
+#define DEBUG_ERROR(str) void()
+#else
+void __debug_print(const char* c) {
+	u64 str_size = 0;
+	while (c[str_size++] != 0)
+		/**/;
+
+	sys_write(0, c, str_size);
+	sys_write(0, "\n", 1);
 }
 
-void* heap_alloc(struct context* context, u64 size) {
-
+void print_cstring(const char *str) {
+	u64 i = 0;
+	while(str[i++] != 0) ;
+	sys_write(0, str, i);
 }
 
-i64 sys_read(u64 fd, const char *buf, size_t count) {
-	i64 ret = 0;
- 	__asm__ volatile (
- 	        "movq $0, %%rax\n\t"
- 	        "movq %1, %%rdi\n\t"
- 	        "movq %2, %%rsi\n\t"
- 	        "movq %3, %%rdx\n\t"
-		"syscall\n\t"
-		"movq %%rax, %0 \n\t"
- 		 : "=rm" (ret)
- 		 : "rm"  (fd), "rm" (buf), "rm" (count)
- 		 : "rax" , "rdi", "rsi", "rdx" /* These two idk */, "r11", "rcx"
- 	       );
-	return ret;
-}
+void print_expression(const struct ExpressionT* expression) {
+	if (!expression) {
+		print_cstring("[NULL PTR]");
+		return;
+	}
+	switch(expression->expr_type) {
+	case STRING:
+		{
+			u64 size = expression->value_string.size;
+			sys_write(0, expression->value_string.content, size);
+			break;
 
-
-
-void parse_expression(const char* buf, u64 count, struct context ctx) {
-	for (int i = 0; i < count || buf[i] != 0; i++) {
-		if (i == )
-
+		}
+	default:
+		print_cstring("[NOT_IMPLEMENTED]");
 	}
 
 }
 
+#define DEBUG_ERROR(str) __debug_print(str)
+#endif
+
+
+void* stack_push_var(struct context* ctx, u64 size) {
+	if ((size % 8) != 0) {
+		return NULL; // Unaligned alloc, not supported
+	}
+	if (ctx->_stack_pointer < size) {
+		return NULL; // We are out of memory
+	}
+
+	ctx->_stack_pointer -= size;
+	return &(ctx->stack[ctx->_stack_pointer]);
+}
+
+/** Push value on stack and return pointer to value
+ */
+void* stack_push_u64(struct context* ctx, u64 value) {
+	u64* addr = stack_push_var(ctx, sizeof(value));
+	if (addr == NULL)
+		return NULL;
+	*addr = value;
+	return addr;
+}
+
+void* heap_alloc(struct context* context, u64 size) {
+	return stack_push_var(context, size); // Temp hack
+}
+
+
+
+i32 parser_find_next_char(const char* buf, u64 start_position, u64 count) {
+	for (u64 i = start_position; i < count; i++) {
+		switch(buf[i]) {
+			case ' ':
+			case '\t':
+			case '\n':
+				continue;
+			default:
+				return i;
+		}
+	}
+	return -1;
+}
+
+Expression* parser_parse_string(
+		const char* buf,
+		u64 max_size,
+		/* out */ u64* num_processed,
+		struct context* ctx) {
+	Expression *ret = NULL;
+	if (max_size < 2 || buf[0] != '"') {
+		DEBUG_ERROR("Wrong string to parse");
+		return NULL;
+	}
+	u64 final_size = 0;
+	u32 found_end = FALSE;
+	// Calculate expected size after parsing
+	// TODO: Skip over special symbols
+	for (u64 i = 1; i < max_size; i++) {
+		if (buf[i] == '"') {
+			found_end = TRUE;
+			break;
+		}
+		final_size++;
+	}
+
+	if (!found_end) {
+		DEBUG_ERROR("String is not ended properly");
+		return NULL;
+	}
+
+	ret = heap_alloc(ctx, sizeof(Expression) + final_size);
+	if (!ret) {
+		DEBUG_ERROR("Allocation failed");
+		return ret;
+	}
+
+	ret->expr_type = STRING;
+
+
+	// Copy string
+	u64 processed = 1;
+	for (u64 i = 1, dest = 0; i < max_size; i++) {
+		processed++;
+		if (buf[i] == '"') {
+			// Now breaking here is guaranteed
+			break;
+		}
+		ret->value_string.content[dest++] = buf[i];
+	}
+	ret->value_string.size = final_size;
+	if (num_processed)
+		*num_processed = processed;
+	return ret;
+}
+
+struct ExpressionT* parser_parse_expression(const char* buf, u32 count, struct context* ctx) {
+	Expression* ret = NULL;
+	for (i64 i = 0; i < count || buf[i] != 0; i++) {
+		char c = buf[i];
+		if (c == '(') { // Begin of expression
+			// int j = i+1;
+			ret = heap_alloc(ctx, sizeof(Expression));
+			if (!ret) {
+				DEBUG_ERROR("Memory fail - parser");
+				return NULL;
+			}
+
+			ret->expr_type = FUNCTION;
+			
+			while (1) { // Load everythin inside expression
+				i = parser_find_next_char(buf,i+1, count);
+				if (i < 0) {
+					// TODO: Free memory
+					DEBUG_ERROR("Cannot find closing brace");
+					return ret;
+
+				}
+				if (buf[i] == ')') { // Parsing finished
+					return ret;
+				}
+
+				Expression* es = parser_parse_expression(buf+i, count - i, ctx);
+				return es;
+			}
+		} else if (c == '"')  {
+			u64 string_size = 0;
+			struct ExpressionT* es =
+				parser_parse_string(buf+i, count-i, &string_size, ctx);
+			if (es == NULL) {
+				DEBUG_ERROR("Invalid string parse");
+			}
+			i += string_size - 1;
+			return es;
+		}
+	}
+	DEBUG_ERROR("Wrong");
+	return NULL;
+}
+
+
 /** Parse string defined inside "". 
  * Return -1 on failure, parsed string size on success
  */
-int parse_string(
+int _parser_parse_string(
 	const char* src, size_t max_size_src, char* dst, size_t max_size_dst) {
 	//--
 	if (max_size_src < 2 || src[0] != '"') {
@@ -92,16 +305,14 @@ good:
 	return dest_write;
 }
 
-int execute(const char* program, char* return_value, size_t size){
-
-}
 
 
-int main() {
-	char command[2048] = {'a', 'h', 'o', 0};
-	sys_read(1, command, 2048);
-	command[2047] = '\0';
-	printf("%s\n", command);
-	//execute(command, )
-
+void _start() {
+	char command[] = {'"', '1','2','h','4','h','6','h','8', '"' };
+	struct context ctx;
+	init_context(&ctx);
+	Expression* ex = parser_parse_expression(command, 10, &ctx);
+	print_expression(ex);
+	//sys_write(0, ex->value_string.content, 8);
+	sys_exit(17);
 }
